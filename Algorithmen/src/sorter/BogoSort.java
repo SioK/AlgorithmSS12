d335a357f04f2a0a3d99c5bd704fbfadd0598d1e
Semo <semox78@gmail.com>
2012-04-27 13:01:56 +0200

    MergeSort Fixed.

---
+++ Algorithmen/src/sorter/BogoSort.java
@@ -0,0 +1,15 @@
+package sorter;
+
+import java.util.List;
+
+import interfaces.ISorter;
+
+public class BogoSort<T extends Comparable<T>> implements ISorter<T> {
+
+  @Override
+  public void sort(List<T> comparables) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
